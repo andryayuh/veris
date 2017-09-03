3cdada01536a263319055173a9405fae998e67fe
Andrea <andreamiralles686@gmail.com>
2017-09-03 11:51:45 -0700

    move up nested json dir 65f-

--- react-client/src/components/Table.jsx
+++ react-client/src/components/Table.jsx
@@ -4,7 +4,7 @@ import ListItem from './TableItem.jsx';
 const Table = (props) => (
   <div>
     There are { props.items.length } items.
-    { props.items.map(item => <ListItem item={item}/>)}
+    { props.items.map(item => <ListItem item={item} key={item}/>)}
   </div>
 );
 
